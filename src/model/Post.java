a3a3664647dc7373a382b20752c6012d9ecd19a9
Daniils Dominiks Štrāls <s24stradani@vea.lv>
2026-03-17 09:43:43 +0200

    Edit: added methods and variables for Post class

--- src/model/Post.java
+++ src/model/Post.java
@@ -1,21 +1,21 @@
 package model;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class Post {
 	private String msg;
-	private Date date;
+	private LocalDateTime datetime;
 	private int countOfLikes;
 	
 	public Post() {
 		msg = "";
-		date = new Date();
+		datetime = LocalDateTime.now();
 		countOfLikes = 0;
 	}
 	
 	public Post(String msg) {
-		this.msg = msg;
-		date = new Date();
+		setMsg(msg);
+		datetime = LocalDateTime.now();
 		countOfLikes = 0;
 	}
 
@@ -24,23 +24,27 @@ public class Post {
 	}
 
 	public void setMsg(String msg) {
-		this.msg = msg;
+		if(msg != null && !msg.isEmpty() && msg.length() < 1000) {
+			this.msg = msg;
+		} else {
+			this.msg = "";
+		}
 	}
 
-	public Date getDate() {
-		return date;
+	public LocalDateTime getDate() {
+		return datetime;
 	}
 
-	public void setDate(Date date) {
-		this.date = date;
+	public void setDate() {
+		datetime = LocalDateTime.now();
 	}
 
 	public int getCountOfLikes() {
 		return countOfLikes;
 	}
 
-	public void setCountOfLikes(int countOfLikes) {
-		this.countOfLikes = countOfLikes;
+	public void incrementCountOfLikes() {
+		countOfLikes++;
 	}
 
 	@Override
